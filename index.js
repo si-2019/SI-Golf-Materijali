bb3288471260c04519c7666286d9a9e1eb1bea10
Alma Hodzic <almahodzic071@outlook.com>
2019-05-12 22:02:51 +0200

    swagger
    Dodan swagger - primjer get i post za dodavanje i uzimanje iz baze materijala

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var express = require('express')
 var app = express()
 var mysql = require('mysql');
 const db = require('./models/db');
+const endpoints = require('./endpoints.js');
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDocument = YAML.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +20,16 @@ db.sequelize.sync().then(function(){
     console.log("Neuspjesno povezivanje");
     console.log(err);
 });
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+//endpoints(app,db);
 
 app.get('/', function(req, res) {
     res.send('GOLF')
