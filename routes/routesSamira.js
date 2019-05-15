5706e4c1670168202822fbe54b5124c778cb6d30
smamela <smamela2@etf.unsa.ba>
2019-05-15 22:40:55 +0200

    vracanje liste Moji predmeti za studenta

--- routes/routesSamira.js
+++ routes/routesSamira.js
@@ -47,6 +47,21 @@ router.get('/mojiPredmeti/:idKorisnik', function(req,res){
             res.end(JSON.stringify(greska));
         });
     }
+    else if(uloga=='STUDENT'){
+        let predmeti=[];
+        db.mojiPredmeti.findAll({attributes:['idKorisnik','idPredmet'],where:{idKorisnik:idKorisnik}}).then(function(korisnici){
+           for(let i=0;i<korisnici.length;i++){
+                db.predmet.findOne({attributes:['id','naziv','opis'],where:{id:korisnici[i].idPredmet}}).then(function(p){
+                    let pr = {id:p.id,naziv:p.naziv,opis:p.opis};
+                    predmeti.push(pr);
+                    if(i==korisnici.length-1){
+                        res.writeHead(200,{"Content-Type":"application/json"});
+                        res.end(JSON.stringify(predmeti));
+                    }
+                });
+            }
+        })
+    }
 
 
 })
