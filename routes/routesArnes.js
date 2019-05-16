ff034270c06e7ae54dc5a27041dd1874d82b8c73
arogo1 <arnes_rogo@hotmail.com>
2019-05-16 21:40:40 +0200

    Programiranje klika na predmet

--- routes/routesArnes.js
+++ routes/routesArnes.js
@@ -6,6 +6,29 @@ router.get('/',function(req,res){
     res.send('Arnes');
 });
 
+router.get('/dajPrivilegije/:idKorisnika/:idPredmeta',function(req,res){
+    let idKorisnika = req.params.idKorisnika;
+    
+    let idPredmeta = req.params.idPredmeta;
+    console.log(idPredmeta);
+    db.predmet.findOne({where :{id:idPredmeta}}).then(function(p){
+        console.log("ušao u if");
+        if(p.idAsistent == idKorisnika || p.idProfesor == idKorisnika || p.idAsistent == null || p.idProfesor == null) {
+            
+            let odg = {privilegija:1}
+            res.end(JSON.stringify(odg));
+        }
+        else{
+            
+            let odg = {privilegija:0}
+            res.end(JSON.stringify(odg));
+        }
+    }).catch(function(err){
+        let greska = {privilegija:2}
+        res.end(JSON.stringify(greska))
+        });
+})
+
 
 
 module.exports = router;
