b397b5305f43771a446995d1535aa09c23cfd4d4
amujak1 <amujak1@etf.unsa.ba>
2019-05-18 15:25:59 +0200

    Ruta za provjeru da li je predmet vec dodan u korisnikovu sekciju moji predmeti

--- routes/routesAmila.js
+++ routes/routesAmila.js
@@ -6,6 +6,24 @@ router.get('/',function(req,res){
     res.send('Amila');
 });
 
+router.get('/provjera/:idKorisnika/:idPredmeta',function(req,res){
+   
+    let idKorisnika = req.params.idKorisnika;
+    let idPredmeta = req.params.idPredmeta;
+
+    db.mojiPredmeti.findAll({attributes:['idKorisnik','idPredmet'],where:{ idKorisnik: idKorisnika, idPredmet: idPredmeta }}).then(rez => {
+        if (rez[0] == null) {
+            let odgovor = { veza: 0 }
+            res.end(JSON.stringify(odgovor));
+        }
+        else {
+            let odgovor = { veza: 1 }
+            res.end(JSON.stringify(odgovor));
+        }
+    });
+});
+
+
 
 
 module.exports = router;
