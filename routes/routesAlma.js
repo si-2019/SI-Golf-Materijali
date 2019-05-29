417adab8e97d6def788b21db1909600853d7bd7d
Alma Hodzic <almahodzic071@outlook.com>
2019-05-29 07:02:50 +0200

    Update routesAlma.js
    Dodana ruta za dohvaćanje akademske godine - za prikaz prethodnih ak godina pri odabiru materijala

--- routes/routesAlma.js
+++ routes/routesAlma.js
@@ -6,6 +6,27 @@ router.get('/',function(req,res){
     res.send('Alma');
 });
 
+router.get('/getAkademskaGodina/', function(req, res){
 
+    let datumPocetkaAG;
+    db.akademskaGodina.findOne({
+        where: {
+            aktuelna: '1'
+        }
+    }).then(function(ag){
+
+        datumPocetkaAG = new Date(ag.pocetak.zimskog_semestra)
+    }
+    let prethodne2AG = []
+    for(let i = 1; i < 3; ++i){
+        prethodne2AG.push({
+            prethodna: datumPocetkaAG.getYear() - i
+        })
+    }
+    res.json({getAkademskaGodina:prethodne2AG})
+    )}.catch(function(err){
+        res.json({message: 'error'})
+    })
+})
 
 module.exports = router;
